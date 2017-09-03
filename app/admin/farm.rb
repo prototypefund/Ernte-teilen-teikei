b3eb34758979a9d41d91124f5c262b36893d2a38
Simon Jockers <hello@simonjockers.com>
2017-09-03 21:32:18 +0200

    Admin: Add specific fields to farm `show` function.

--- app/admin/farm.rb
+++ app/admin/farm.rb
@@ -24,4 +24,81 @@ ActiveAdmin.register Farm do
     column :name
     column :location
   end
+
+  show do
+    panel 'Name and Description' do
+      table do
+        tr do
+          td 'Name'
+          td farm.name
+        end
+        tr do
+          td 'URL'
+          td farm.url
+        end
+        tr do
+          td 'Description'
+          td farm.description
+        end
+      end
+    end
+
+    panel 'Location' do
+      table do
+        tr do
+          td 'City'
+          td farm.city
+        end
+        tr do
+          td 'Address'
+          td farm.address
+        end
+        tr do
+          td 'Latitude / longitude'
+          td [farm.latitude, farm.longitude].join(' / ')
+        end
+      end
+    end
+
+    panel 'Details' do
+      table do
+        tr do
+          td 'Vegetable products'
+          td farm.vegetable_products.join(', ')
+        end
+        tr do
+          td 'Animal products'
+          td farm.animal_products.join(', ')
+        end
+        tr do
+          td 'Beverages'
+          td farm.beverages.join(', ')
+        end
+        tr do
+          td 'Additional product information'
+          td farm.additional_product_information
+        end
+        tr do
+          td 'Economical behavior'
+          td farm.economical_behavior
+        end
+        tr do
+          td 'Founded at'
+          td [farm.founded_at_month, farm.founded_at_year].join('/')
+        end
+        tr do
+          td 'Accepts new members'
+          td farm.accepts_new_members
+        end
+        tr do
+          td 'Maximum_members'
+          td farm.maximum_members
+        end
+        tr do
+          td 'Participation'
+          td farm.participation
+        end
+      end
+    end
+  end
 end
